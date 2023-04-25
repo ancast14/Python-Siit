dddba53687ce9e16965fe56054e6ff054414bdba
Anca <truta.anca.simina@gmail.com>
2023-04-25 13:08:05 +0300

    updated

--- Python Basics/my_homeworks/tema5/main.py
+++ Python Basics/my_homeworks/tema5/main.py
@@ -1,27 +1,39 @@
+import numpy as np
+
+
 class Fraction:
     def __init__(self, numerator, denominator):
-        self._numerator = numerator
-        self._denominator = denominator
+        self.__numerator = numerator
+        self.__denominator = denominator
 
-    # def simplifier(self):
-    #     self.
+    def simplify(self):
+        gcd = self.__gcd(self.__numerator, self.__denominator)
+        self.__numerator //= gcd
+        self.__denominator //= gcd
+
+    def __gcd(self, a, b):
+        if b == 0:
+            return a
+        else:
+            return self.__gcd(b, a % b)
 
     def __str__(self):
-        if self._numerator == 0:
+        self.simplify()
+        if self.__numerator == 0:
             return "Are you sure you want to divide zero by this denominator?"
-        if self._denominator == 0:
+        if self.__denominator == 0:
             return "You cannot divide by zero."
         else:
-            return f"{self._numerator} / {self._denominator}"
+            return f"{self.__numerator} / {self.__denominator}"
 
 
     @property
     def numerator(self):
-        return self._numerator
+        return self.__numerator
 
     @property
     def denominator(self):
-        return self._denominator
+        return self.__denominator
 
     def __add__(self, other):
         new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
@@ -38,10 +50,26 @@ class Fraction:
 
 
 if __name__ == '__main__':
-    fraction1 = Fraction(0, 2)
+    fraction1 = Fraction(2, 0)
     fraction2 = Fraction(3, 4)
     print(fraction1)
     print(fraction2)
     print(fraction1 + fraction2)
     print(fraction2 - fraction1)
     print(fraction1.invert())
+
+    fraction3 = Fraction(0, 2)
+    fraction4 = Fraction(3, 4)
+    print(fraction3)
+    print(fraction4)
+    print(fraction3 + fraction4)
+    print(fraction4 - fraction3)
+    print(fraction3.invert())
+
+    fraction5 = Fraction(2, 4)
+    fraction6 = Fraction(3, 4)
+    print(fraction5)
+    print(fraction6)
+    print(fraction5 + fraction6)
+    print(fraction6 - fraction5)
+    print(fraction5.invert())
